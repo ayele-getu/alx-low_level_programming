917238d86beede44ddad1667403c4dfb4cdd63b9
Ayele-Getu-Gebremariyam <ayelegetu44@gmail.com>
2022-04-08 11:04:01 -0700

    This is the commit for all tasks of the '0x01. C - Variables, if, else, while' project

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -9,12 +9,12 @@ int main(void)
 {
 int digit;
 
-for (digit = 0 ; digit < 10; digit++)
+for (digit = 0; digit < 10; digit++)
 {
 printf("%digit", digit);
 }
 
-printf("\n")
+printf("\n");
 
 return (0);
 
