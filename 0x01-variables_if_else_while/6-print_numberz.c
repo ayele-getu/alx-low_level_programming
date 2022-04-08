c2077d1bbcd1fe638c1a4d7798f0d3ac53050da0
Ayele-Getu-Gebremariyam <ayelegetu44@gmail.com>
2022-04-08 11:17:03 -0700

    This is the commit for all tasks of the '0x01. C - Variables, if, else, while' project

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio>
+/**
+ * main - Empty point
+ *
+ * Return: Always 0 (Success/correct)
+ */
+int main(void)
+
+{
+int i;
+
+for (i = 48; i <= 57; i++)
+{
+puchar (i);
+}
+
+putchar ("\n");
+
+return (0);
+
+}
