09c527316d745f4bffcf86f4a54d0b177b39f846
Ayele-Getu-Gebremariyam <ayelegetu44@gmail.com>
2022-04-08 07:40:22 -0700

    This is the commit for all tasks of the '0x01. C - Variables, if, else, while' project

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,8 +6,8 @@
  */
 int main(void)
 
-char upper_case;
 char lower_case;
+char upper_case;
 
 for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
 {
@@ -16,7 +16,7 @@ putchar(lower_case);
 
 for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
 {
-putchar(lower_case);
+putchar(upper_case);
 }
 
 putchar('\n');
