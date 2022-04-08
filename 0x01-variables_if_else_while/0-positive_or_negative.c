8f2db3fe9dc4484933a89763ca9ac0673c16558d
Ayele-Getu-Gebremariyam <ayelegetu44@gmail.com>
2022-04-08 06:10:18 -0700

    This is the commit for all tasks of the '0x01. C - Variables, if, else, while' project

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,18 +7,16 @@
  *
  * Return: Always 0 (Success/correct)
  */
-
 int main(void)
 {
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-       if (n==0)
+       if	(n == 0)
        {
-		printf("%i is zero\n", n);
-	}
-       else if (n<0)
+	       printf("%i is zero\n", n);
+       }
+       else if (n < 0)
        {
 	       printf("%i is negative\n", n);
        }
@@ -26,6 +24,5 @@ int main(void)
        {
 	       printf("%i is positive\n", n);
        }
-
        return (0);
 }
