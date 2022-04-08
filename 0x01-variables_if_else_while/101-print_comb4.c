d61c9dbd194b8612ab9cf449eca6aa527cb2e5d6
Ayele-Getu-Gebremariyam <ayelegetu44@gmail.com>
2022-04-08 12:34:58 -0700

    This is the commit for all tasks of the '0x01. C - Variables, if, else, while' project

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,7 +6,6 @@
  * Return: Always 0 (Success/correct)
  */
 int main(void)
-
 {
 int digit1, digit2;
 
@@ -14,8 +13,8 @@ for (digit1 = 0; digit1 < 9; digit1++)
 {
 for (digit2 = digit1 + 1; digit2 < 10; digit2++)
 {
-putchar((digit1 % 10)+'0');
-putchar((digit2 % 10)+'0');
+putchar((digit1 % 10) + '0');
+putchar((digit2 % 10) + '0');
 
 if (digit1 == 8 && digit2 == 9)
 continue;
