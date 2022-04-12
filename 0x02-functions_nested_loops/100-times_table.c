4875249727e6c041ceecd6260f0c08d0c706401a
Ayele-Getu-Gebremariyam <ayelegetu44@gmail.com>
2022-04-12 11:31:55 -0700

    This is the commit for '0x02. C - Functions, nested loops' project

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,6 +8,7 @@
 
 void print_times_table(int n)
 {
+
 	int num, mult, prod;	
 
 	if (n >= 0 && n <= 15)
@@ -20,6 +21,7 @@ void print_times_table(int n)
 			{
 				_putchar(',');
 				_putchar(' ');				
+
 				prod = num * mult;
 				if (prod <= 99)
 					_putchar(' ');
