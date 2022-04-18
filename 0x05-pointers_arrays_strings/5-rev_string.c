30764eb1eb67d7b30a0f66d15d3d0a008f019cbb
Ayele-Getu-Gebremariyam <ayelegetu44@gmail.com>
2022-04-18 04:49:25 -0700

    This is the commit for '0x05. C - Pointers, arrays and strings' project

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,14 +10,14 @@ void rev_string(char *s)
 {
 	int length, j, i;
 	char v1, v2;
-	
+
 	for (length = 0; s[length] != '\0'; length++)
 	{
 	}
-	
+
 	j = length - 1;
 	i = 0;
-	
+
 	while (j > i)
 	{
 		v1 = s[i];
