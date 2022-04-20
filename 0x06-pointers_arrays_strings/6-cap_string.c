b1ae80607ee6dbe73ae49728f515ffbb02a9ab3a
Ayele-Getu-Gebremariyam <ayelegetu44@gmail.com>
2022-04-20 12:29:47 -0700

    This is the commit for '0x06. C - More pointers, arrays and strings' project

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,40 +1,36 @@
 #include "main.h"
 
 /**
- * cap_string - capitalizes all words of a string.
- *
- * @s: string to use.
- * Return: string.
+ * cap_string - function that capitalizes all words of a string.
+ * @s: Pointer to Char
+ * Return: char.
  */
 
 char *cap_string(char *s)
 {
-	int i = 1, j, check;
-	char a[] = {',', ';', '.', '!', '?', '"', '(', ')', '{', '}', '\n', '\t', ' '};
+	int i = 0;
 
-	if (s[0] > 96 && s[0] < 123)
-		s[0] -= 32;
-
-	while (s[i] != '\0')
+	while (*(s + i) != '\0')
 	{
-		if (s[i] > 96 && s[i] < 123)
+		if (i == 0 && (*(s + i) >= 97 && *(s + i) <= 122))
 		{
-			j = 0;
-			check = 0;
-			while (check == 0 && j < 13)
+			*(s + i) = *(s + i) - ' ';
+			i++;
+		}
+		if (*(s + i) == ' ' || *(s + i) == '\n' || *(s + i) == '\t'
+				|| *(s + i) == ',' || *(s + i) == ';' || *(s + i) == '!'
+				|| *(s + i) == '?' || *(s + i) == '"' || *(s + i) == '('
+				|| *(s + i) == ')' || *(s + i) == '{' || *(s + i) == '}'
+				|| *(s + i) == '.')
+		{
+			i++;
+			if (*(s + i) >= 97 && *(s + i) <= 122)
 			{
-				if (s[i - 1] == a[j])
-				{
-					check = 1;
-				}
-				j++;
-			}
-			if (check == 1)
-			{
-				s[i] -= 32;
+				*(s + i) = *(s + i) - ' ';
 			}
 		}
-		i++
+		else
+			i++;
 	}
 	return (s);
 }
