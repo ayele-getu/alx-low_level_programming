edd9f9c8e7bbae9274bf44960b57e2a0a5656b72
Ayele-Getu-Gebremariyam <ayelegetu44@gmail.com>
2022-04-27 10:05:07 -0700

    This is the commit for all tasks of '0x07. C - Even more pointers, arrays and strings' project

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -15,6 +15,7 @@ void print_diagsums(int *a, int size)
 	int sum, sum2;
 
 	sum = sum2 = 0;
+
 	while (i < (size * size))
 	{
 		value = *(a + i);
@@ -22,11 +23,13 @@ void print_diagsums(int *a, int size)
 		i = i + size + 1;
 	}
 	i = size - 1;
+
 	while (i < ((size * size) - 1))
 	{
 		value = *(a + i);
 		sum2 = sum2 + value;
 		i = i + (size - 1);
 	}
+
 	printf("%d, %d\n", sum, sum2);
 }
