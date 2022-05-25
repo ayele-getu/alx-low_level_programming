652eb5c53ac8f3cf72870de8d7cdcd6ec1990a02
Ayele-Getu-Gebremariyam <102233285+Ayele-Getu-Gebremariyam@users.noreply.github.com>
2022-05-25 11:16:48 +0300

    Mandatory Task 4 commit

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,2 +1,24 @@
-jdhhdb
-hdg
+#include "lists.h"
+/**
+ * free_list - free a list
+(* a blank line
+ *@head: the head of list
+* Description: this function free a list.)?
+(* section header: the header of this function is lists.h)*
+* Return: void, no return
+ */
+void free_list(list_t *head)
+{
+	list_t *position, *next;
+
+	position = head;
+
+	while (position != '\0')
+	{
+		next = position->next;
+		free(position->str);
+		free(position);
+		position = next;
+	}
+	head = '\0';
+}
