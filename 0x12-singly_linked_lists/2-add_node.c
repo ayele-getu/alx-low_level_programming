4b424e10b2a80f476d61d6f39676823458d92bfa
Ayele-Getu-Gebremariyam <102233285+Ayele-Getu-Gebremariyam@users.noreply.github.com>
2022-05-25 11:25:55 +0300

    Mandatory Task 2 commit

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,50 +1,45 @@
 #include "lists.h"
+
+int _strlen_recursion(char *s);
+
 /**
- * add_node - add a new node to a list
-(* a blank line
- *@head: the head of list
- *@str: the string to put in the new node
-* Description: add a new node to a list)?
-(* section header: the header of this function is lists.h)*
-* Return: the head of the list.
- */
+ * add_node - adds a new node at the beginning of a list_t list.
+ * @head: pointer to the first element of the list.
+ * @str: string to set in the new node.
+ * Return: address of the new element, or NULL if it failed
+ **/
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *node;
-	char *_str;
+	list_t *new;
 
-	if (str == NULL)
-		return (NULL);
-	node = malloc(sizeof(list_t));
-	if (node == NULL)
-		return (NULL);
-	_str = strdup(str);
-	if (_str == NULL)
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
 	{
-		free(node);
 		return (NULL);
 	}
-	node->len = _strlen(_str);
-	node->str = _str;
-	node->next = *head;
-	*head = node;
-	return (*head);
+
+	new->str = strdup(str);
+	if (!new->str)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->len = _strlen_recursion(new->str);
+	new->next = *head;
+	*head = new;
+
+	return (new);
 }
+
 /**
- * _strlen - this functions prints the lenght of a string
- (* a blank line
- *@s: string to print.
- * Description: this function prints the lenght of a string?
- (* section header: the header of this function is holberton.h)*
- * Return: retunrn the lenght in int
+ * _strlen_recursion - returns the length of a string.
+ * @s: string.
+ * Return: length of @s.
  */
-int _strlen(char *s)
+int _strlen_recursion(char *s)
 {
-	if (*s != '\0')
-	{
-		s++;
-		return (1 + _strlen(s));
-	}
-	else
+	if (*s == 0)
 		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
