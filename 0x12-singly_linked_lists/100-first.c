453ab42d4548871b034846ea7be1b5504c9e3c2d
Ayele-Getu-Gebremariyam <102233285+Ayele-Getu-Gebremariyam@users.noreply.github.com>
2022-05-25 11:13:04 +0300

    Advanced Task 5 commit

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,2 +1,11 @@
-uurjj
-jfh
+#include <stdio.h>
+/**
+ * premain - execute code before the main
+ * description - execute code before the main
+ * return - non return, void function
+ */
+void __attribute__ ((constructor)) premain(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
